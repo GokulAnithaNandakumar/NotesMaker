bc8c797426fd6ecf6a2271cf255867bffb96318c
GokulAnithaNandakumar <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2025-02-05 21:31:59 +0530

    commit

--- Frontend/notes-app/src/utils/constants.js
+++ Frontend/notes-app/src/utils/constants.js
@@ -1,2 +1,2 @@
 // src/utils/constants.js
-export const BASE_URL = 'http://localhost:8000'; // Ensure this is correct for your environment
+export const BASE_URL = process.env.BASE_URL; // Ensure this is correct for your environment
